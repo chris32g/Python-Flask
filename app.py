b3683fd846e2ffc414c93a2371e7b8491f5aeeaf
Christian <chris32p@gmail.com>
2020-04-30 08:54:32 +0000

    added SQLAlchemy database

--- app.py
+++ app.py
@@ -1,28 +1,15 @@
-from flask import Flask, jsonify, Response
-from flask import request
+from flask import Flask, jsonify,request, Response
+from bookmodel import *
+from settings import *
 import json
 import requests
-app = Flask(__name__)
-
-
-books = [
-    {
-        'name': 'book1',
-        'price': 7.99,
-        'isbn': 5468416516
-    },
-    {
-        'name': 'book2',
-        'price': 19.95,
-        'isbn': 32165486
-    }
-]
+from json import JSONEncoder
 
 
 # GET all books
 @app.route('/')
-def hello_world(): 
-        return jsonify({'books': books})
+def get_books():
+    return jsonify({'books': Book.list_all_books()})
 
 def valid_book(book):
     if "name" in book and "price" in book and "isbn" in book:
@@ -39,7 +26,7 @@ def add_book():
                     'price': request_data['price'],
                     'isbn': request_data['isbn']
                     }
-        books.insert(0, new_book)
+        Book.add_book(request_data['name'], request_data['price'], request_data['isbn'])
         response = Response("", 201, mimetype = 'application/json')
         response.headers['Location'] = "/books/" + str(new_book['isbn'])
         return response
@@ -52,18 +39,9 @@ def add_book():
 def replace_book(isbn):
     requested_book = request.get_json()
     if "name" in requested_book and "price" in requested_book:
-        updated_book = {
-            'name': requested_book['name'],
-            'price': requested_book['price'],
-            'isbn': requested_book['isbn']
-        }
-        i=0
-        for book in books:
-            if book['isbn'] == isbn:
-                books[i] = updated_book
-            i += 1
-            response = Response("", status=204)
-            return response
+        Book.replace_book(isbn , requested_book['name'], requested_book['price'])
+        response = Response("", status=204)
+        return response
     else:
         response = Response("Invalid values", status=400)
         return response
@@ -73,45 +51,32 @@ def replace_book(isbn):
 @app.route('/books/<int:isbn>', methods = ['PATCH'])
 def patch_book(isbn):
     request_data = request.get_json()
-    updated_book = {}
     if 'name' in request_data:
-        updated_book['name'] = request_data['name']
+        Book.update_book_name(isbn , request_data['name'])
+        response = Response("", status = 204)
+        response.headers['Location']= '/books/' + str(isbn)
+        return response
     if 'price' in request_data:
-        updated_book['price'] = request_data['price']
-    if 'isbn' in request_data:
-        updated_book['isbn'] = request_data['isbn']    
-    for book in books:
-        if book['isbn'] == isbn:
-            book.update(updated_book)
-            response = Response("", status = 204)
-            response.headers['Location']= '/books/' + str(book['isbn'])
-            return response
+        Book.update_book_price(isbn , request_data['price'])   
+        response = Response("", status = 204)
+        response.headers['Location']= '/books/' + str(isbn)
+        return response
 
 
 # DELETE book by isbn
 @app.route('/books/<int:isbn>', methods = ['DELETE'])
 def delete_book(isbn):
-    i = 0
-    for book in books:
-        if book['isbn'] == isbn:
-            books.pop(i)
-            response = Response("", status = 204)
-            return response
-        i += 1
-    invalid_isbn_message = {"error" : "a book with the inserted isbn was not found"}
-    response = Response(json.dumps(invalid_isbn_message), status = 400, mimetype='application/json')
+    if(Book.delete(isbn)):
+        return Response("", status=204)
+    else:
+        invalid_isbn_message = {"error" : "a book with the inserted isbn was not found"}
+        response = Response(json.dumps(invalid_isbn_message), status = 400, mimetype='application/json')
     return response
 
 # GET book by isbn
 @app.route('/books/<int:isbn>')
 def book_isbn(isbn):
-    return_value = {}
-    for book in books:
-        if book["isbn"] == isbn:
-            return_value = {
-                'name': book["name"],
-                'price': book["price"]
-            } 
+    return_value = Book.get_book(isbn)
     return (jsonify(return_value))
 
 
